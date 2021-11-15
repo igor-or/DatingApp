b7a3a8f18074c386be628ef1612e1ee2def0f1cd
Ihor Oryshchenko <igor89.or@gmail.com>
2021-11-15 22:47:57 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +55,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
